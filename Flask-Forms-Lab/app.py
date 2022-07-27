dc25c03bc96a95fad957751a56491b6dc35fcd97
AsilGa23-meet <109899267+AsilGa23-meet@users.noreply.github.com>
2022-07-27 12:31:28 +0300

    asil

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -13,11 +13,17 @@ password = "123"
 facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
 
 
-@app.route('/')  # '/' for the default page
+@app.route('/', methods=['GET', 'POST'])
 def login():
-  return render_template('login.html')
-  
+	if request.method == 'GET':
+		  return render_template('login.html')
+	else:
+		if(request.form("username")==username and request.form("password")==password):
+			return render_template('home.html')
 
+@app.route('/home')
+def home():
+	return render_template('home.html')
 
 
 if __name__ == "__main__":  # Makes sure this is the main process
